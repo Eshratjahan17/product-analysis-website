cac687ddedb133f14125870dcbd6360e8480f8b3
Eshratjahan17 <eshratjahan701@gmail.com>
2022-04-05 15:57:01 +0600

    404 page done

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import img from '../../utilities/images/404.jpg';
 
 const NotFound = () => {
   return (
     <div>
-      <h1>404</h1>
+      <img src={img}
+      ></img>
     </div>
   );
 };
